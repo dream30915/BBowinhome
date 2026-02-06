f9386be138e00c7a81551b49770e982834c06299
BBowinhome Admin <admin@bbowinhome.com>
2026-02-07 04:32:00 +0700

    fix: retry deployment of massive logo and contrast fixes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
         <nav className={styles.navbar}>
             <div className={styles.container}>
                 <Link href="/" className={styles.logo}>
-                    <img src="/logo.jpg" alt="BBowinhome Logo" style={{ height: '120px', objectFit: 'contain' }} />
+                    <img src="/logo.jpg" alt="BBowinhome Logo" style={{ height: '150px', objectFit: 'contain' }} />
                 </Link>
                 <div className={styles.links}>
                     <Link href="#services" className={styles.link}>บริการของเรา</Link>
